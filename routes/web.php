942e9731f9f6f5aa8876efa63a4279b34f9bc248
agungleksono <agungleksono99@gmail.com>
2025-07-28 10:44:07 +0700

    delete unuse route

--- routes/web.php
+++ routes/web.php
@@ -30,12 +30,6 @@ Route::post('/signin', [AuthController::class, 'loginWeb']);
 Route::post('/signout', [AuthController::class, 'logoutWeb'])->name('logout');
 
 Route::get('/test', [SandboxController::class, 'test']);
-// Route::get('/users/management', [UserController::class, 'index']);
-// Route::get('/users/{id}/edit', [UserController::class, 'edit']);
-// Route::put('/users/{user_id}', [UserController::class, 'update'])->name('user.update');
-// Route::post('/users', [UserController::class, 'store']);
-// Route::post('/suspect/import', [SuspectController::class, 'importSuspects']);
-// Route::post('/suspect/import', [SuspectImportController::class, 'import']);
 
 Route::middleware(['auth.web'])->group(function () {
     Route::get('/', [SuspectImportController::class, 'index']);
