c196bb01aa0fa8cf0d80560038d458febd72ce7e
agungleksono <agungleksono99@gmail.com>
2025-08-04 10:34:25 +0700

    delete comment code in web.php

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,6 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-// Route::get('/', function () {
-//     return view('pages.login');
-// });
-
 Route::get('/', [AuthController::class, 'loginPage'])->name('login')->middleware('guest.web');
 Route::post('/signin', [AuthController::class, 'loginWeb']);
 Route::post('/signout', [AuthController::class, 'logoutWeb'])->name('logout');
@@ -32,7 +28,6 @@ Route::post('/signout', [AuthController::class, 'logoutWeb'])->name('logout');
 Route::get('/test', [SandboxController::class, 'test']);
 
 Route::middleware(['auth.web'])->group(function () {
-    // Route::get('/', [SuspectImportController::class, 'index']);
     Route::get('/suspects', [SuspectImportController::class, 'index'])->name('suspects.index');
     Route::post('/suspects/import', [SuspectImportController::class, 'import'])->name('suspects.import');
     Route::post('/suspect/manual-add', [SuspectImportController::class, 'manualAdd'])->name('suspects.manual-add');
